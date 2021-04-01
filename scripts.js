45b923f090ed0bafad0be12ba5183429bc123f1f
mhyneman8 <mhyneman@gmail.com>
2021-04-01 10:29:25 -0500

    Task 1.9 index not responding

--- scripts.js
+++ scripts.js
@@ -1,32 +1,52 @@
+function container () {
+
+  let container = document.querySelector('.container');
+  let addButton = document.createElement('button');
+  addButton.addClass('button');
+  addButton.innerText = "Add New List";
+  container.appendChild(addButton);
+
+  addButton.addEventListener('click', function() {
+    newItem();
+  })
+}
+container();
 
 function newItem(){
 
 //javascript
 //1. Adding a new item to the list of items:
-   let li = document.createElement("li");
-   let inputValue = document.getElementById("input").value;
-   let text = document.createTextNode(inputValue);
-   li.append(text);
+   let li = $(<li></li>);
+   let inputValue = $('input').val();
+   $(li).append(inputValue);
+
+
+   function handle(e) {
+     if(e.keyCode === 13) {
+     let li = $(<li></li>);
+     let inputValue = $('input').val();
+     $(li).append(inputValue);
+   } return true;
+ }
+
 
    if (inputValue === '') {
-     alert("You must write something!");
+     alert("help!");
    } else {
-     let list = document.querySelector('#list');
-     list.append(li);
+     $('#list').append(li);
    }
-
  //2. Crossing out an item from the list of items:
    function crossOut() {
- 		li.classList.toggle("strike");
+ 		li.toggleClass("strike");
  	}
 
   $(li).on('dblclick', crossOut);
 
 
  //3(i). Adding the delete button "X":
-   let crossOutButton = document.createElement("crossOutButton");
+   let crossOutButton = $("<crossOutButton></crossOutButton>");
  	crossOutButton.append(document.createTextNode("X"));
- 	li.append(crossOutButton);
+  li.append(crossOutButton);
 
   $(crossOutButton).on('click', deleteListItem);
 
